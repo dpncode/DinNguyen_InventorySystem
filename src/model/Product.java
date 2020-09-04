47e7ddb4813a513aefec9ab843464acc60708642
dpncode <70594032+dpncode@users.noreply.github.com>
2020-09-04 06:06:00 -0400

    model and view controllers

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,10 +1,182 @@
 package model;
 
+import javafx.beans.InvalidationListener;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
-public class Product {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
-    - associatedParts:ObservableList<Part>
+public class Product {
+   ObservableList<Part> associatedParts = new ObservableList<Part>() {
+       @Override
+       public void addListener(ListChangeListener<? super Part> listener) {
+
+       }
+
+       @Override
+       public void removeListener(ListChangeListener<? super Part> listener) {
+
+       }
+
+       @Override
+       public boolean addAll(Part... elements) {
+           return false;
+       }
+
+       @Override
+       public boolean setAll(Part... elements) {
+           return false;
+       }
+
+       @Override
+       public boolean setAll(Collection<? extends Part> col) {
+           return false;
+       }
+
+       @Override
+       public boolean removeAll(Part... elements) {
+           return false;
+       }
+
+       @Override
+       public boolean retainAll(Part... elements) {
+           return false;
+       }
+
+       @Override
+       public void remove(int from, int to) {
+
+       }
+
+       @Override
+       public int size() {
+           return 0;
+       }
+
+       @Override
+       public boolean isEmpty() {
+           return false;
+       }
+
+       @Override
+       public boolean contains(Object o) {
+           return false;
+       }
+
+       @Override
+       public Iterator<Part> iterator() {
+           return null;
+       }
+
+       @Override
+       public Object[] toArray() {
+           return new Object[0];
+       }
+
+       @Override
+       public <T> T[] toArray(T[] a) {
+           return null;
+       }
+
+       @Override
+       public boolean add(Part part) {
+           return false;
+       }
+
+       @Override
+       public boolean remove(Object o) {
+           return false;
+       }
+
+       @Override
+       public boolean containsAll(Collection<?> c) {
+           return false;
+       }
+
+       @Override
+       public boolean addAll(Collection<? extends Part> c) {
+           return false;
+       }
+
+       @Override
+       public boolean addAll(int index, Collection<? extends Part> c) {
+           return false;
+       }
+
+       @Override
+       public boolean removeAll(Collection<?> c) {
+           return false;
+       }
+
+       @Override
+       public boolean retainAll(Collection<?> c) {
+           return false;
+       }
+
+       @Override
+       public void clear() {
+
+       }
+
+       @Override
+       public Part get(int index) {
+           return null;
+       }
+
+       @Override
+       public Part set(int index, Part element) {
+           return null;
+       }
+
+       @Override
+       public void add(int index, Part element) {
+
+       }
+
+       @Override
+       public Part remove(int index) {
+           return null;
+       }
+
+       @Override
+       public int indexOf(Object o) {
+           return 0;
+       }
+
+       @Override
+       public int lastIndexOf(Object o) {
+           return 0;
+       }
+
+       @Override
+       public ListIterator<Part> listIterator() {
+           return null;
+       }
+
+       @Override
+       public ListIterator<Part> listIterator(int index) {
+           return null;
+       }
+
+       @Override
+       public List<Part> subList(int fromIndex, int toIndex) {
+           return null;
+       }
+
+       @Override
+       public void addListener(InvalidationListener listener) {
+
+       }
+
+       @Override
+       public void removeListener(InvalidationListener listener) {
+
+       }
+   };
+/*    - associatedParts:ObservableList<Part>
 - id : int
 - name : String
 - price : double
@@ -27,7 +199,7 @@ public class Product {
 + getMax():int
 + addAssociatedPart(part:Part):void
 + deleteAssociatedPart(selectedAssociatedPart:Part):boolean
-+ getAllAssociatedParts():ObservableList<Part>
++ getAllAssociatedParts():ObservableList<Part>*/
 
 
     public Product(int id) {
