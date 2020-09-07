cb744ce39ac1da16c16123500907f3b365565022
dpncode <70594032+dpncode@users.noreply.github.com>
2020-09-07 06:42:39 -0400

    addparts fields and buttons

--- src/view_controller/InhouseController.java
+++ src/view_controller/InhouseController.java
@@ -1,7 +1,36 @@
 package view_controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class InhouseController {
+    //creating textfield so that it can be called through any method
+    //@fxml is used to fethch the fields throught the given id
+   @FXML
+   private TextField nameFld,inventoryFld,priceFld,maxFld,machineFld,minFld;
     public void addInhouse(){
+
+    }
+
+    public void saveInhousePart(MouseEvent mouseEvent) {
+        InhouseController ic = new InhouseController(); //arraylist
+        //testing value entries
+        System.out.println(nameFld.getText());
+        System.out.println(inventoryFld.getText());
+        System.out.println(priceFld.getText());
+        System.out.println(maxFld.getText());
+        System.out.println(machineFld.getText());
+        System.out.println(minFld.getText());
+        //Todo: save inhouse part
+        String name = nameFld.getText();
+        String inventory = inventoryFld.getText();
+      //  InHouse inh = new InHouse(11,)
+        //send the vvalues to partcontroller
+        //create a refernce of PartController her
+        PartController pc = new PartController();
+        pc.getValues(nameFld.getText(),inventoryFld.getText(),priceFld.getText(),maxFld.getText(),machineFld.getText(),minFld.getText());
+
 
     }
 }
--- src/view_controller/PartController.java
+++ src/view_controller/PartController.java
@@ -1,4 +1,41 @@
 package view_controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class PartController {
+
+    //this method is called whenever mouse is clicked onthe respective button in our case add button of mainsceeen
+    public void showInhouse(MouseEvent mouseEvent) throws IOException {
+        //the method to redirect to addInhourpart.fxml file
+        //root is the reference f the parent class to fetch the fxml file and load when the program starts
+        //getresoucre is the method to get the fxml file
+        //getclass fetch the class which contains the fxml file in our case it is mainscreen file
+        // load is to get all the code into gui from Fxmlloader class
+        // all the data is stored in root which is to be shown from class Stage
+        Parent root = FXMLLoader.load(getClass().getResource("/view_controller/add_InhousePart.fxml"));
+        //primarystage is the reference of stage class to prepage a location to show the fxml file
+        Stage primaryStage = new Stage(); //
+        //a setscene is the method to set the fxml desing to  the primary stage
+        primaryStage.setScene(new Scene(root));
+        //show is the method to show the loaded file
+        primaryStage.show();
+
+    }
+//this method gets the value of inhouse part from Inhouse controller
+    public void getValues(String text, String text1, String text2, String text3, String text4, String text5) {
+        System.out.println("these are the values from partcontroller ");
+        System.out.println(text);
+        System.out.println(text1);
+        System.out.println(text2);
+        System.out.println(text3);
+        System.out.println(text4);
+        System.out.println(text5);
+        //TODO: to show values to the mainscreen
+    }
 }
